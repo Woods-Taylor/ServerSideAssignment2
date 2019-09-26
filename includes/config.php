fadcac28957222e7caf6cf118f3ef2d3ac908487
woods-taylor <43648944+Woods-Taylor@users.noreply.github.com>
2019-09-26 15:13:46 -0500

    added PHP demo files

---
+++ includes/config.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Used to store website configuration information.
+ *
+ * @var string or null
+ */
+function config($key = '')
+{
+    $config = [
+        'name' => 'Taylors Go Website',
+        'pretty_uri' => false,
+        'site_url' => 'https://serversidephp.herokuapp.com/',
+        'nav_menu' => [
+            '' => 'Home',
+            'about-me' => 'About Me',
+            'image-gallery' => 'Image Gallery'
+        ],
+        'template_path' => 'template',
+        'content_path' => 'content',
+        'version' => 'v1.0',
+    ];
+    return isset($config[$key]) ? $config[$key] : null;
+}
