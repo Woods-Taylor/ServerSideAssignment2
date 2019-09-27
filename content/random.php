01a2dd82455b06f142be2c04f516dd2c22ce915f
woods-taylor <43648944+Woods-Taylor@users.noreply.github.com>
2019-09-26 23:04:47 -0500

    adding normal html links

--- content/random.php
+++ content/random.php
@@ -10,10 +10,13 @@
     <link rel="stylesheet" href="template/template.php">
 <body>
 
+
+
 <h2>Different Types of Go Boards</h2>
 <p>Go is considered by many to be the oldest board game in the world that is still played. It is thought to be somewhere between 2,000 and over 4,000 years old.
     The object of the game is for two players to compete to surround the most territory by alternating placing black or white stones one at a time.
 </p>
+<?php getComic(); ?>
 <div id = "gallery">
 <img src="boardWithStones.jpg" class="mediumPicture"/>
 <a href='https://photos.google.com/share/AF1QipOj8S-MGrIJLj9HgVWNkjQtdp7arx1rtvIjOhDCqplZ-3VUkTjfF5GfhO4YV38-Sw?key=TEtLR0hGY1p2MWFGQUwtcXdldDRUblhzWDJrQmlR&source=ctrlq.org'><img src='https://lh3.googleusercontent.com/v-kONDMSk-hswmpnLdxuhy9M1CMAY_Tf4l3heflHi3HNNKkA3AmIUHRq_lbnZw3018CKxgOf32NcKeh0eCnEaFDjqIIGl9H7EE3Ah7Ll1jiBXwPwoChqCdigD7zY4AUlV5dRJO5w=w2400' class="mediumPicture"/></a>
