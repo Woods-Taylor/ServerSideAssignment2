15ae9e6a888b6052da0c38a4aeb8295839245602
woods-taylor <43648944+Woods-Taylor@users.noreply.github.com>
2019-10-01 01:47:20 -0500

    fixing links

--- content/home.php
+++ content/home.php
@@ -1,6 +1,5 @@
 
 <h1>Taylor's Website</h1>
-</p>Welcome to my web page. Here you can learn a little bit about the amazing game of go. There is an example game below.</p>
 
-
+<?php page_content(); ?>
 <?php getComic(); ?>
