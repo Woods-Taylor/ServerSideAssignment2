22a49f331e6c87c570d42e0061abf2a7d3576268
woods-taylor <43648944+Woods-Taylor@users.noreply.github.com>
2019-12-02 19:47:13 -0500

    test

--- content/home.php
+++ content/home.php
@@ -1,4 +1,17 @@
 <article>
 <?php getComic(); ?>
+<?PHP
+$a="hello";
+?>
+<script>
+function echoComic(){
+ alert("<?PHP getRandomComic(); ?>");
+ }
+</script>
+
+<?PHP
+<div>
+  <button onclick="echoComic()">Random Comic</button>
+</div>
 <?php getRandomComic(); ?>
 </article>
